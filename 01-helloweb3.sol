d8b5b5927080e94ed06e5e551e5381e9ef712446
0xMilo <myhuang342@gmail.com>
2025-09-22 19:36:56 +0800

    Initialize Solidity exercise

---
+++ 01-helloweb3.sol
@@ -0,0 +1,8 @@
+//注释仍然是两条杠
+pragma solidity ^0.8.21;
+//pragma:编译指示，第二个条件由^提供
+contract HelloWeb3{
+    string public _string="Hello Web3!";//定义一个公共字符串变量，并初始化
+    /*contract:创建合约，并声明合约名为helloweb3
+    内容：声明了string变量，并赋值为“hello web3"*/
+}
