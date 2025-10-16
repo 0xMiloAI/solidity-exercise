f8bd6ace962d04355e78fffaaaf770c59b48b3e8
0xMilo <myhuang342@gmail.com>
2025-10-16 08:23:02 +0800

    seventeenth commit

---
+++ 17-Library.sol
@@ -0,0 +1,80 @@
+pragma solidity ^0.8.21;
+/*库合约：，为了提升Solidity代码的复用性和减少gas而存在；
+他和普通合约主要有以下几点不同：
+不能存在状态变量
+不能够继承或被继承
+不能接收以太币
+不可以被销毁*/
+library Strings {
+    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
+     */
+    function toString(uint256 value) public pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT licence
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
+    }
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation.
+     */
+    function toHexString(uint256 value) public pure returns (string memory) {
+        if (value == 0) {
+            return "0x00";
+        }
+        uint256 temp = value;
+        uint256 length = 0;
+        while (temp != 0) {
+            length++;
+            temp >>= 8;
+        }
+        return toHexString(value, length);
+    }
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
+     */
+    function toHexString(uint256 value, uint256 length) public pure returns (string memory) {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = _HEX_SYMBOLS[value & 0xf];
+            value >>= 4;
+        }
+        require(value == 0, "Strings: hex length insufficient");
+        return string(buffer);
+    }
+}
+contract Library{
+    //使用库合约
+    //1.利用using for指令
+    using Strings for uint256;
+    //2.调用库函数
+    function printUint(uint256 _number) public pure returns(string memory){
+        //如果要接收返回值就要用变量来接收
+        return _number.toHexString();
+    }
+    //还可以用库合约名直接调用
+    function uintToString(uint256 _number) public pure returns(string memory){
+        return Strings.toString(_number);
+    }
+}
